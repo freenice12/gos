96f75cab35ca682e956eb4c4f04dfcf6180af681
freenice12 <freenice12@gmail.com>
2018-01-08 12:04:14 +0900

    close issue #1

--- src/main/java/com/example/demo/members/MembershipController.java
+++ src/main/java/com/example/demo/members/MembershipController.java
@@ -1,5 +1,6 @@
 package com.example.demo.members;
 
+import com.example.demo.commons.errors.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class MembershipController {
@@ -20,11 +23,15 @@ public class MembershipController {
     }
 
     @PostMapping("/memberships")
-    public ResponseEntity createMembership(@RequestBody MembershipDTO.Create create,
+    public ResponseEntity createMembership(@RequestBody @Valid MembershipDTO.Create create,
                                            BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<>(new RuntimeException("something wrong!"), HttpStatus.BAD_REQUEST);
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.setMessage("잘못된 요청입니다. (" + result.getFieldError().getField() + " : " + result.getFieldError().getDefaultMessage() + ")");
+            errorResponse.setCode("bad.request");
+            // TODO BindingResult 안에 들어있는 에러 정보 사용하기.
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
         Membership m = membershipService.createMembership(create);
         System.out.println(m);
--- src/test/java/com/example/demo/members/MembershipControllerITest.java
+++ src/test/java/com/example/demo/members/MembershipControllerITest.java
@@ -6,24 +6,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class MembershipControllerITest extends DemoApplicationTests {
 
@@ -47,24 +42,24 @@ public class MembershipControllerITest extends DemoApplicationTests {
     }
 
     @Test
-    @WithMockUser
     public void test_get_memberships_should_return_empty() throws Exception {
         ResultActions resultActions = mvc
-                .perform(get("/memberships").with(httpBasic("id", "pwd")))
+                .perform(get("/memberships")
+//                        .with(httpBasic("id", "pwd"))
+                )
                 .andExpect(status().isOk());
 
         resultActions.andDo(print());
     }
 
     @Test
-    public void test_post_memberships() throws Exception {
+    public void test_post_memberships_create_success() throws Exception {
         MembershipDTO.Create create = new MembershipDTO.Create();
         create.setEmail("tester@gmail.com");
         create.setPassword("test");
         create.setAdmin(true);
 
         ResultActions result = mvc.perform(post("/memberships")
-                .with(httpBasic("id", "pwd"))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(create)));
 
@@ -73,6 +68,42 @@ public class MembershipControllerITest extends DemoApplicationTests {
         result.andExpect(jsonPath("$.id", is(1)));
         result.andExpect(jsonPath("$.email", is("tester@gmail.com")));
         result.andExpect(jsonPath("$.password", is("test")));
+    }
+
+    @Test
+    public void test_post_memberships_create_fail_empty_password() throws Exception {
+        MembershipDTO.Create create = new MembershipDTO.Create();
+        create.setEmail("tester1@gmail.com");
+        String emptyPassword = null;
+        create.setPassword(emptyPassword);
+
+        System.out.println("=========================");
+        System.out.println(objectMapper.writeValueAsString(create));
+        ResultActions result = mvc.perform(post("/memberships")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(create)));
+
+        result.andDo(print());
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message", is("잘못된 요청입니다. (password : may not be empty)")));
+        result.andExpect(jsonPath("$.code", is("bad.request")));
+    }
+
+    @Test
+    public void test_post_memberships_create_fail_short_password() throws Exception {
+        MembershipDTO.Create create = new MembershipDTO.Create();
+        create.setEmail("tester@gmail.com");
+        String shortPassword = "123";
+        create.setPassword(shortPassword);
+
+        ResultActions result = mvc.perform(post("/memberships")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(create)));
+
+        result.andDo(print());
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$.message", is("잘못된 요청입니다. (password : size must be between 4 and 30)")));
+        result.andExpect(jsonPath("$.code", is("bad.request")));
     }
 
 }
